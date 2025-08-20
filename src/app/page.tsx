9fbe871b587b24088d6c92bf58d6f1e789c9ea78
vaujan <vaujan@github.com>
2025-08-20 22:21:32 +0700

    update import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Sidebar from "../components/ui/sidebar";
 import LeftCard from "../components/ui/left-card";
 import RightCard from "../components/ui/right-card";
 import Dock from "../components/ui/dock";
-import Stats from "../components/ui/stats";
+import Stats from "../components/ui/session-table";
 import DeepWork from "../components/ui/deepwork";
 
 export default function Page() {
 	return (
 		<div className="min-h-screen bg-base-300 flex flex-col lg:flex-row">
-			<Sidebar />
+			{/* <Sidebar /> */}
 			<main className="w-full relative flex flex-col gap-6 flex-1 items-center justify-start ">
 				{/* Container */}
 				<div className="max-w-8xl flex flex-col gap-6 w-full px-8 py-16">
