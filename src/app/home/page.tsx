e73dabf3802c7dbe4ce4e9201aaddc2170762ed7
vaujan <vaujan@github.com>
2025-08-21 14:28:43 +0700

    readjust layout

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,25 +15,22 @@ export default function page() {
 				<div className="flex w-full h-fit flex-col">
 					<div className="max-w-8xl items-start h-full flex gap-6 w-full px-8 py-8">
 						<SessionCard />
-						<div className="flex flex-col">
-							{/* <SessionGraph /> */}
-							<SessionTable />
-						</div>
+						<SessionTable />
 					</div>
 				</div>
 
 				{/* Widgets layout */}
 				<div className="min-h-screen w-full justify-center items-center flex flex-col gap-4 ">
 					{/* Kanban widget */}
-					<div className="flex flex-col w-full h-fit px-4">
+					{/* <div className="flex flex-col w-full h-fit px-4">
 						<div className="w-full h-128 rounded-box bg-base-100 border border-base-100"></div>
-					</div>
+					</div> */}
 
 					{/* Notes and tasks widget */}
-					<div className="flex w-full gap-4 h-fit px-4">
+					{/* <div className="flex w-full gap-4 h-fit px-4">
 						<div className="w-full h-128 rounded-box bg-base-100 border border-base-100"></div>
 						<div className="w-full h-128 rounded-box bg-base-100 border border-base-100"></div>
-					</div>
+					</div> */}
 				</div>
 				<div className="font-medium sticky w-32 bottom-10">
 					<Dock />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import DeepWork from "../components/ui/deepwork";
 export default function Page() {
 	return (
 		<div className="min-h-screen bg-base-300 flex flex-col lg:flex-row">
-			{/* <Sidebar /> */}
+			<Sidebar />
 			<main className="w-full relative flex flex-col gap-6 flex-1 items-center justify-start ">
 				{/* Container */}
 				<div className="max-w-8xl flex flex-col gap-6 w-full px-8 py-16">
