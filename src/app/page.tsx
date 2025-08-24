70bc23291000e2470b3ddc188743e6894f54c099
Fauzan <139192843+vaujan@users.noreply.github.com>
2025-08-24 15:48:11 +0700

    reroute to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,37 @@
+import Dock from "@/src/components/ui/dock";
+import SessionCard from "@/src/components/ui/session-card";
+import Sidebar from "@/src/components/ui/sidebar";
+import SessionTable from "@/src/components/ui/session-table";
+import useWidgets from "@/src/hooks/useWidgets";
 import React from "react";
-import Sidebar from "../components/ui/sidebar";
-import LeftCard from "../components/ui/left-card";
-import RightCard from "../components/ui/right-card";
-import Dock from "../components/ui/dock";
-import Stats from "../components/ui/session-table";
-import DeepWork from "../components/ui/deepwork";
-import TauriDemo from "../components/ui/tauri-demo";
+import SessionGraph from "@/src/components/ui/session-graph";
 
 export default function Page() {
 	return (
 		<div className="min-h-screen bg-base-300 flex flex-col lg:flex-row">
 			<Sidebar />
-			<main className="w-full relative flex flex-col gap-6 flex-1 items-center ustify-start ">
-				{/* Container */}
-				<div className="max-w-8xl flex flex-col gap-6 w-full px-8 py-16">
-					<div className="w-full gap-8 flex">
-						{/* Card */}
-						<div className="flex flex-col gap-8">
-							<LeftCard />
-							<DeepWork />
-						</div>
-						{/*  Card */}
-						<div className="flex flex-col gap-8">
-							<Stats />
-							<RightCard />
-						</div>
-					</div>
-
-					<div className="w-full flex gap-8">
-						{/* Card */}
-						<Stats />
-						{/* Tauri Demo */}
-						<TauriDemo />
+			{/* Container */}
+			<main className="w-full relative flex flex-col gap-4 flex-1 items-center justify-start">
+				<div className="flex w-full h-full flex-col">
+					<div className="max-w-8xl items-start justify-center h-full flex gap-6 w-full px-8 py-8">
+						<SessionCard />
+						{/* <SessionTable /> */}
 					</div>
 				</div>
 
+				{/* Widgets layout */}
+				<div className="min-h-screen w-full justify-center items-center flex flex-col gap-4 ">
+					{/* Kanban widget */}
+					{/* <div className="flex flex-col w-full h-fit px-4">
+						<div className="w-full h-128 rounded-box bg-base-100 border border-base-100"></div>
+					</div> */}
+
+					{/* Notes and tasks widget */}
+					{/* <div className="flex w-full gap-4 h-fit px-4">
+						<div className="w-full h-128 rounded-box bg-base-100 border border-base-100"></div>
+						<div className="w-full h-128 rounded-box bg-base-100 border border-base-100"></div>
+					</div> */}
+				</div>
 				<div className="font-medium sticky w-32 bottom-10">
 					<Dock />
 				</div>
---
+++ src/app/test/page.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Sidebar from "../../components/ui/sidebar";
+import LeftCard from "../../components/ui/left-card";
+import RightCard from "../../components/ui/right-card";
+import Dock from "../../components/ui/dock";
+import Stats from "../../components/ui/session-table";
+import DeepWork from "../../components/ui/deepwork";
+import TauriDemo from "../../components/ui/tauri-demo";
+
+export default function Page() {
+	return (
+		<div className="min-h-screen bg-base-300 flex flex-col lg:flex-row">
+			<Sidebar />
+			<main className="w-full relative flex flex-col gap-6 flex-1 items-center ustify-start ">
+				{/* Container */}
+				<div className="max-w-8xl flex flex-col gap-6 w-full px-8 py-16">
+					<div className="w-full gap-8 flex">
+						{/* Card */}
+						<div className="flex flex-col gap-8">
+							<LeftCard />
+							<DeepWork />
+						</div>
+						{/*  Card */}
+						<div className="flex flex-col gap-8">
+							<Stats />
+							<RightCard />
+						</div>
+					</div>
+
+					<div className="w-full flex gap-8">
+						{/* Card */}
+						<Stats />
+						{/* Tauri Demo */}
+						<TauriDemo />
+					</div>
+				</div>
+
+				<div className="font-medium sticky w-32 bottom-10">
+					<Dock />
+				</div>
+			</main>
+		</div>
+	);
+}
