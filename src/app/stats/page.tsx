f946fd7bfe70aedc794a9a9a658f810c710eb024
Fauzan <139192843+vaujan@users.noreply.github.com>
2025-09-09 11:36:37 +0700

    add stats header

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Sidebar from "../../components/ui/sidebar";
-import Header from "../../components/ui/header";
+import StatsHeader from "../../components/ui/stats-header";
 import StatsOverview from "../../components/ui/stats-overview";
 import SessionHistory from "../../components/ui/session-history";
 import HeatMap from "../../components/ui/heat-map";
@@ -17,15 +17,18 @@ export default function StatsPage() {
 			<Sidebar />
 			{/* Main Content */}
 			<main className="w-full relative flex flex-col gap-4 flex-1 items-start justify-start">
-				<Header />
+				<StatsHeader />
 				<div className="flex w-full h-full flex-col">
 					<div className="w-full h-full flex justify-center items-center overflow-x-auto horizontal-scroll-container">
-						<div className="flex max-w-[1440px] flex-col gap-4 w-full h-full px-4 lg:px-8 py-4">
+						<div
+							id="stats-content"
+							className="flex max-w-[1440px] flex-col gap-4 w-full h-full px-4 lg:px-8 py-4"
+						>
 							{/* Stats Overview Cards */}
 							<div className="card flex-col lg:flex-row gap-4">
 								<HeatMap />
 
-								<div className="flex h-full flex-col w-full gap-4">
+								<div className="flex h-full  flex-col w-full gap-4">
 									<FocusStreak />
 									<StatsOverview />
 								</div>
