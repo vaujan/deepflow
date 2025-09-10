15573332bc06effd09c8f317578dffdeb3eb84a4
Ahmad Fauzan <buildfrombed@gmail.com>
2025-09-11 00:56:33 +0700

    wip: mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,14 @@ export default function Page() {
 		<div className="min-h-screen bg-base-300 flex flex-col lg:flex-row">
 			<Sidebar />
 			{/* Container */}
-			<main className="w-full relative overflow-hidden flex flex-col gap-4 flex-1 items-start justify-start">
+			<main className="w-full h-full relative overflow-hidden flex flex-col gap-4 flex-1 items-start justify-start">
 				<Header />
 				<div className="flex w-full h-full flex-col">
 					<div className="w-full h-full overflow-x-auto horizontal-scroll-container">
 						<div
-							className={`flex flex-col lg:flex-row gap-8 items-start ${
-								activeWidgets.length > 1
-									? "justify-start min-w-fit"
-									: "justify-center w-full"
-							} h-full px-4 lg:px-8 py-4`}
+							className={`flex flex-col lg:flex-row ${
+								activeWidgets.length > 1 ? "justify-start" : "justify-center"
+							} w-full items-start gap-8 px-4 lg:px-8 py-4`}
 						>
 							{/* Session Card - Always visible */}
 							<SessionCard />
@@ -35,12 +33,15 @@ export default function Page() {
 							{(activeWidgets.includes("note") ||
 								activeWidgets.includes("tasks") ||
 								activeWidgets.includes("kanban")) && (
-								<div className="flex flex-row  gap-8 w-full h-fit min-w-fit">
+								<div
+									className={`flex flex-row rounded-box w-full border-border border md:bg-pink-500 lg:bg-cyan-500 p-4 md:w-fit md:p-0 md:border-0 overflow-x-auto gap-8 h-fit`}
+								>
 									{activeWidgets.includes("note") && <WidgetNotes />}
 									{activeWidgets.includes("tasks") && <WidgetTask />}
 									{/* {activeWidgets.includes("kanban") && <WidgetKanban />} */}
 								</div>
 							)}
+							{/* <div className="w-xl h-xl bg-red-500">hello world</div> */}
 						</div>
 					</div>
 				</div>
