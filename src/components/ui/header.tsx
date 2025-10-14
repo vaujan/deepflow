449fbed9e415c9f10e90f4dfcab7b3e4f2b16ae6
vaujan <vaujan@github.com>
2025-10-14 19:41:18 +0900

    remove sync status

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,6 @@
 import { AreaChartIcon } from "lucide-react";
 import Profile from "./profile";
 import WidgetList from "./widget-list";
-import SyncStatus from "./sync-status";
-import Link from "next/link";
 
 export default function Header() {
 	return (
@@ -12,7 +10,6 @@ export default function Header() {
 					{/* active widgets count */}
 					<WidgetList />
 					<div className="flex gap-4 items-center">
-						<SyncStatus />
 						<Profile />
 						{/*  window control for when it opened in electron. minim */}
 					</div>
